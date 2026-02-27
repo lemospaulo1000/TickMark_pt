4e386f183ffd1e15feb91b770669df0d58b7e35b
lemospaulo1000 <lemospaulo1000@gmail.com>
2026-02-27 15:22:36 -0300

    Reorganiza documentação para fora do projeto VSTO

--- AuditorAddInRibbon/My Project/AssemblyInfo.vb
+++ AuditorAddInRibbon/My Project/AssemblyInfo.vb
@@ -22,7 +22,7 @@ Imports System.Security
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("2533761a-0600-4ad7-bb70-de5a5eb70891")> 
+<Assembly: Guid("2533761a-0600-4ad7-bb70-de5a5eb70891")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -35,8 +35,8 @@ Imports System.Security
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
 
 Friend Module DesignTimeConstants
     Public Const RibbonTypeSerializer As String = "Microsoft.VisualStudio.Tools.Office.Ribbon.Serialization.RibbonTypeCodeDomSerializer, Microsoft.VisualStudio.Tools.Office.Designer, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
